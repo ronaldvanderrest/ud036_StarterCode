e682c899d4681df3e01e3660f1a02540a6b5ecdd
Ronald van der Rest <ronaldvanderrest@MacBook-Pro-van-Ronald.local>
2017-10-29 11:11:32 +0100

    Write python program to generate Movie webpage

---
+++ media.py
@@ -0,0 +1,14 @@
+class Movie():
+    """This class is used for creating Movie objects containing information regarding title, posterURL and trailerURL.
+
+        Attributes:
+            movie_title: A string containing the Movie's title.
+            poster_image_url: A string containing the URL to the Movie's poster.
+            trailer_youtube_url: A string containing the URL to the Movie's trailer.
+    """
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """Inits Movie with movie_title, movie_poster, movie_trailer"""
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
